aaf3b38b3ab89f40d09cf06922fc0326c1697e74
vasylenko-dmytro <dmytro.m.vasylenko@gmail.com>
2026-02-06 14:40:42 +0100

    refactor unit tests

--- backend/ecollecto-backend/src/main/java/com/vasylenko/ecollectobackend/common/model/BaseDocument.java
+++ backend/ecollecto-backend/src/main/java/com/vasylenko/ecollectobackend/common/model/BaseDocument.java
@@ -1,5 +1,6 @@
 package com.vasylenko.ecollectobackend.common.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
@@ -8,5 +9,6 @@ import org.springframework.data.annotation.Id;
 @Setter
 public abstract class BaseDocument {
     @Id
+    @JsonProperty("_id")
     protected String id;
 }
