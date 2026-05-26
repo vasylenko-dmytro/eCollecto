eb3f244e3ebb93965b93dc97c447a050daf3afad
vasylenko-dmytro <dmytro.m.vasylenko@gmail.com>
2026-05-26 14:50:22 +0200

    add a convention for intentionally-unused bindings

--- frontend/ecollecto-ui/eslint.config.js
+++ frontend/ecollecto-ui/eslint.config.js
@@ -19,5 +19,16 @@ export default defineConfig([
       ecmaVersion: 2022,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+        },
+      ],
+    },
   },
 ])
